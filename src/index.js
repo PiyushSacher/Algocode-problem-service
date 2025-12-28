18849c9236b8aa2cf2231a3a566605d1ec87de48
Piyush Sacher <piyushsacher18@gmail.com>
2025-12-28 19:21:52 +0530

    Added mvc structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express=require("express");
 const {PORT}=require("./config/server.config")
 const app=express();
-
+const apiRouter=require("./routes/index")
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+
+app.use("/api",apiRouter)
 
 app.listen(PORT,()=>{
     console.log(`Server listening on port ${PORT}`);
