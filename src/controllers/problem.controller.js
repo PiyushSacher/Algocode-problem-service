a2546a370713327e8bdb85e55c9073d5ba4661a9
Piyush Sacher <piyushsacher18@gmail.com>
2025-12-29 11:45:23 +0530

    Feat: Added error handling middleware with basic error classes

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,25 +1,30 @@
-const {StatusCodes} =require("http-status-codes")
+const {StatusCodes} =require("http-status-codes");
+const UnImplemented = require("../errors/unimplemented.error");
 
-function addProblem(req,res){
-    return res.status(StatusCodes.NOT_IMPLEMENTED).json({message:"Not implemented"});
+function addProblem(req,res,next){
+    try{
+        throw new UnImplemented("Add Problem not implemented");
+    }catch(err){
+        next(err);
+    }
 }
 
-function getProblem(req,res){
-    return res.status(StatusCodes.NOT_IMPLEMENTED).json({message:"Not implemented"});
-
-}
-
-function getProblems(req,res){
-    return res.status(StatusCodes.NOT_IMPLEMENTED).json({message:"Not implemented"});
-
-}
-
-function deleteProblem(req,res){
+function getProblem(req,res,next){
     return res.status(StatusCodes.NOT_IMPLEMENTED).json({message:"Not implemented"});
 
 }
 
-function updateProblem(req,res){
+function getProblems(req,res,next){
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({message:"Not implemented"});
+
+}
+
+function deleteProblem(req,res,next){
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({message:"Not implemented"});
+
+}
+
+function updateProblem(req,res,next){
     return res.status(StatusCodes.NOT_IMPLEMENTED).json({message:"Not implemented"});
 
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const express=require("express");
 const {PORT}=require("./config/server.config")
 const app=express();
-const apiRouter=require("./routes/index")
+const apiRouter=require("./routes/index");
+const BaseError = require("./errors/base.error");
+const errorHandler = require("./utils/errorHandler");
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 
 app.use("/api",apiRouter)
 
+
+//last middleware if any error comes
+app.use(errorHandler)
+
 app.listen(PORT,()=>{
     console.log(`Server listening on port ${PORT}`);
+
+    //throw new BaseError("Some Error",404,"Something went wrong")
 })
